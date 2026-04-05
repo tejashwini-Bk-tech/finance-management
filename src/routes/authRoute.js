ce8d28b8a7eed15d6a35a13e407c3e8c0433f4d6
tejashwini-Bk-tech <tejashwinikoli2005@gmail.com>
2026-04-05 22:37:31 +0530

    commit

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,7 +1,7 @@
 import { register, login } from "../controllers/authController.js";
 import { Router } from "express";
 
-export const router = new Router
+export const router = new Router()
 
 router.post("/register", register)
 router.post("/login", login)
